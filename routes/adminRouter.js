fa71bf8f7965a39ac2ebebc2a834ba6e95f93554
adeebmuhammed <muhammedadeeb445@gmail.com>
2024-09-24 12:05:52 +0530

    category management completed

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const adminController = require('../controllers/admin/adminController')
 const {userAuth,adminAuth} = require("../middlewares/auth")
 const customerController = require("../controllers/admin/customerController")
+const categoryController = require("../controllers/admin/categoryController")
 
 router.get("/pageerror",adminController.pageerror)
 // Login Management
@@ -14,5 +15,14 @@ router.get("/logout",adminController.logout)
 router.get("/users",adminAuth,customerController.customerInfo)
 router.get("/blockCustomer",adminAuth,customerController.customerBlocked)
 router.get("/unblockCustomer",adminAuth,customerController.customerunBlocked)
+//Category Management
+router.get("/category",adminAuth,categoryController.categoryInfo)
+router.post("/addCategory",adminAuth,categoryController.addCategory)
+router.post("/addCategoryOffer",adminAuth,categoryController.addCategoryOffer)
+router.post("/removeCategoryOffer",adminAuth,categoryController.removeCategoryOffer)
+router.get("/listCategory",adminAuth,categoryController.getListCategory)
+router.get("/unlistCategory",adminAuth,categoryController.getUnlistCategory)
+router.get("/editCategory",adminAuth,categoryController.getEditCategory)
+router.post("/editCategory/:id",adminAuth,categoryController.editCategory)
 
 module.exports = router

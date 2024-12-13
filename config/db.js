692da95375ad6bb6f80b0894eb4b352844375086
adeebmuhammed <muhammedadeeb445@gmail.com>
2024-12-13 12:19:40 +0530

    new changes

--- config/db.js
+++ config/db.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
-const env = require('dotenv').config();
+require('dotenv').config(); // Load environment variables
 
-const connectDB = async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("DB connected");
-        
-    } catch (error) {
-        console.log("DB connection error",error.message);
-        process.exit(1)
+const connectDB = async () => {
+  try {
+    // Validate the presence of MONGODB_URL in the environment
+    if (!process.env.MONGODB_URL) {
+      throw new Error('MONGODB_URL environment variable is not set');
     }
-}
 
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('DB connected');
+  } catch (error) {
+    console.error('DB connection error:', error.message);
+    process.exit(1); // Exit process with failure code
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;

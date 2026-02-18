f70b85d383857f0049e9423049856de7b3468caf
adeebmuhammed <muhammedadeeb445@gmail.com>
2026-02-18 10:56:20 +0530

    BW pendings solved

--- controllers/user/walletController.js
+++ controllers/user/walletController.js
@@ -3,33 +3,47 @@ const User = require("../../models/userSchema");
 const Cart = require("../../models/cartSchema");
 
 const getWalletPage = async (req, res) => {
-    try {
-        const userId = req.session.user;
-        const userData = await User.findOne({ _id: userId });
-        const wallet = await Wallet.findOne({ user_id: userId });
+  try {
+    const userId = req.session.user;
+    const userData = await User.findById(userId);
 
-       
-        if (!wallet) {
-            const newWallet = await Wallet.create({
-                user_id: userId,
-                balance: 0, 
-                transactions: [],
-            });
+    const page = parseInt(req.query.page) || 1;
+    const limit = 5;
+    const skip = (page - 1) * limit;
 
-            return res.render("wallet", {
-                user: userData,
-                wallet: newWallet,
-            });
-        }
+    let wallet = await Wallet.findOne({ user_id: userId });
 
-        res.render("wallet", {
-            user: userData,
-            wallet,
-        });
-    } catch (error) {
-        res.redirect("/pageNotFound");
+    if (!wallet) {
+      wallet = await Wallet.create({
+        user_id: userId,
+        balance: 0,
+        transactions: [],
+      });
     }
+
+    const totalTransactions = wallet.transactions.length;
+
+    // Slice transactions for current page
+    const paginatedTransactions = wallet.transactions
+      .slice()
+      .reverse() // latest first
+      .slice(skip, skip + limit);
+
+    const totalPages = Math.ceil(totalTransactions / limit);
+
+    res.render("wallet", {
+      user: userData,
+      wallet,
+      transactions: paginatedTransactions,
+      currentPage: page,
+      totalPages,
+    });
+  } catch (error) {
+    console.error(error);
+    res.redirect("/pageNotFound");
+  }
 };
+
 
 module.exports = {
     getWalletPage
